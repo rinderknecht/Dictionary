1422289a0db6f108e587b1a0fd3ac78d9e9149cf
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2020-10-18 16:44:33 +0200

    Upgrade.

--- Dict.ml
+++ Dict.ml
@@ -1,37 +1,37 @@
-(** This module exports two versions of the same abstract data type: a
-    dictionary implemented by means of a ternary search tree.
+(* This module exports two versions of the same abstract data type: a
+   dictionary implemented by means of a ternary search tree.
 
-      In short, a ternary search tree is an optimisation of a {i
-    trie}, where the children of a node are stored in a binary search
-    tree instead of a list.
+     In short, a ternary search tree is an optimisation of a trie,
+   where the children of a node are stored in a binary search tree
+   instead of a list.
 
-      The comparison of a character of the current input key to the
-    character in the current node yields three ways: to the left, if
-    the input character is strictly lower than the node character; to
-    the right if strictly greater, {i and to the middle if equal}.
+     The comparison of a character of the current input key to the
+   character in the current node yields three ways: to the left, if
+   the input character is strictly lower than the node character; to
+   the right if strictly greater, and to the middle if equal.
 
-      This data structure was fist published in {i Fast Algorithms for
-    Searching and Sorting Strings}, J.L. Bentley and R. Sedgewick, in
-    Proceedings of the 8th Annual CAM-SIAM Symposium on Discrete
-    Algorithms, January, 1997.
+     This data structure was fist published in {i Fast Algorithms for
+   Searching and Sorting Strings}, J.L. Bentley and R. Sedgewick, in
+   Proceedings of the 8th Annual CAM-SIAM Symposium on Discrete
+   Algorithms, January, 1997.
 
-      We offer here two implementations in OCaml.
+     We offer here two implementations in OCaml.
 
      One version is provided by the signature [Std] ("standard"),
-    whilst the other by [Opt] ("optimised"), the former enabling
-    insertions, whilst the latter offers path compression (hence
-    faster searches), but no insertions. Conversions between the two
-    representations are provided.
-*)
+   whilst the other by [Opt] ("optimised"), the former enabling
+   insertions, whilst the latter offers path compression (hence faster
+   searches), but no insertions. Conversions between the two
+   representations are provided. *)
 
 type word = string
 type 'def entry = word * 'def
 
-(** {1 Utility functions} *)
+(* Utility functions *)
 
-(** The call [rev_chars l] evaluates in the string made of the
-    characters in the list [l], in reverse order. For example,
-    [rev_chars ['a';'b';'c'] = "cba"]. *)
+(* The call [rev_chars l] evaluates in the string made of the
+   characters in the list [l], in reverse order. For example,
+   [rev_chars ['a';'b';'c'] = "cba"]. *)
+
 let rev_chars (l: char list) : string =
   let len = List.length l in
   let seq = Bytes.create len in
@@ -40,26 +40,23 @@ let rev_chars (l: char list) : string =
   | c::l -> Bytes.set seq i c; make (i-1) l
 in make (len-1) l
 
-(** The call [rcat s] is the string made of the concatenation of the
-    strings in the list [s], in reverse order. For example, [rcat
-    ["ab";"c";"";"def"] = "defcab"]. *)
-let rcat (stack: string list) : string =
+(* The call [rcat s] is the string made of the concatenation of the
+   strings in the list [s], in reverse order. For example, [rcat
+   ["ab";"c";"";"def"] = "defcab"]. *)
 
+let rcat (stack: string list) : string =
   let len = List.fold_left (fun a l -> a + String.length l) 0 stack in
   let seq = Bytes.create len in
-
   let copy j s =
     let rec cp i j =
       if i = -1 then j else (Bytes.set seq j s.[i]; cp (i-1) (j-1))
     in cp (String.length s - 1) j in
-
   let rec make j = function
       [] -> assert (j = -1); Bytes.to_string seq
   | s::l -> make (copy j s) l
-
   in make (len-1) stack
 
-(** {1 The two implementations of ternary search trees} *)
+(* The two implementations of ternary search trees *)
 
 module rec Std : sig
   type 'def t =
@@ -83,28 +80,35 @@ module rec Std : sig
   exception Found of word
 end
 = struct
-  (** A dictionary of type {! t} is implemented as a ternary
-      search tree. *)
+  (* A dictionary of type [t] is implemented as a ternary search
+     tree.
+
+       * [Nil] denotes an empty dictionary.
+
+       * [Pre (d,s)] ("prefix") denotes the dictionary where the empty
+         word is mapped to the definition [d] and other words may be
+         found in the dictionary denoted by the subtree [s].
+
+       * [Leg (c,l,m,r)] ("character, lower, equal, greater") is tree
+         whose root is the character [c], left subtree is [l], middle
+         subtree is [m] and right subtree is [r]:
+
+           - the subtree [l] denotes the dictionary containing words
+             starting with a letter lower than [c], in alphabetic
+             order;
+
+           - the subtree [m] denotes a dictionary containing words
+             such that prefixing them with the character [c], they
+             belong to the dictionary denoted by [Leg (c,l,m,r)];
+
+           - the subtree [r] denotes a dictionary holding words
+             starting with a letter greater than [c], in alphabetic
+             order. *)
+
   type 'def t =
-    Nil   (** [Nil] denotes an empty dictionary. *)
+    Nil
   | Pre of 'def * 'def t
-    (** [Pre (d,s)] ("prefix") denotes the dictionary where the empty
-        word is mapped to the definition [d] and other words may be
-        found in the dictionary denoted by the subtree [s]. *)
   | Leg of char * 'def t * 'def t * 'def t
-    (** [Leg (c,l,m,r)] ("character, lower, equal, greater") is tree
-        whose root is the character [c], left subtree is [l], middle
-        subtree is [m] and right subtree is [r]:
-        {ul
-          {li the subtree [l] denotes the dictionary containing words
-              starting with a letter lower than [c], in alphabetic
-              order;}
-          {li the subtree [m] denotes a dictionary containing words such
-              that prefixing them with the character [c], they belong to
-              the dictionary denoted by [Leg (c,l,m,r)];}
-          {li the subtree [r] denotes a dictionary holding words
-              starting with a letter greater than [c], in alphabetic
-              order.}} *)
 
   type 'def dict = 'def t
 
@@ -115,22 +119,24 @@ end
   let add word def dict =
     let len = String.length word in
 
-    (** The evaluation of the call [mk_suffix n] is a branch of a
-        ternary search tree made of [n] nodes [Leg] with empty
-        side-trees, ended by a node [Pre], holding the the definition
-        [def] captured as an argument of [add] in the scope. *)
+    (* The evaluation of the call [mk_suffix n] is a branch of a
+       ternary search tree made of [n] nodes [Leg] with empty
+       side-trees, ended by a node [Pre], holding the the definition
+       [def] captured as an argument of [add] in the scope. *)
+
     let rec mk_suffix (n: int) =
       if n = 0 then Pre (def,Nil)
       else Leg (word.[len-n], Nil, mk_suffix(n-1), Nil) in
 
-    (** The call [insert n dict] evaluates in a dictionary containing
-        all the entries of dictionary [dict], plus the entry
-        [(word.[len-n..len-1],def)], where [len] is the length of
-        [word] and [word[s..e]] denotes the substring of [word]
-        starting at position [s] and ending at position [e]. Clearly,
-        the call [insert len dict] results in the addition of the
-        entry [(word,def)] to [dict], except if [word] is already in
-        [dict], in which case the exception [Found word] is raised. *)
+    (* The call [insert n dict] evaluates in a dictionary containing
+       all the entries of dictionary [dict], plus the entry
+       [(word.[len-n..len-1],def)], where [len] is the length of
+       [word] and [word[s..e]] denotes the substring of [word]
+       starting at position [s] and ending at position [e]. Clearly,
+       the call [insert len dict] results in the addition of the
+       entry [(word,def)] to [dict], except if [word] is already in
+       [dict], in which case the exception [Found word] is raised. *)
+
     let rec insert (n: int) = function
                      Nil -> mk_suffix n
     |          Pre (d,t) -> if n = 0 then
@@ -161,10 +167,11 @@ end
   let find_opt word dict : 'def option =
     try Some (find word dict) with Not_found -> None
 
-  (** The entries of a dictionary implemented as a ternary search tree
+  (* The entries of a dictionary implemented as a ternary search tree
      are located at the fringe of the tree. The alphabetic order is
      achieved by a leftward post-order traversal, pushing the entries
      in a stack. *)
+
   let entries (dict: 'def t) : 'def entry list =
     let rec post path entries = function
                 Nil -> entries
@@ -185,12 +192,13 @@ end
   |     Pre (_,t) -> suffix c t
   | Leg (x,l,m,r) -> if c = x then m else suffix c (if c < x then l else r)
 
-  (** The call [inorder dict] evaluates in the list of entries in the
-      dictionary [dict], sorted in order, i.e., infix traversal of the
-      underlying ternary search tree. The function [inorder] is useful
-      when an optimal tree is designed and the corresponding entries
-      are needed in the proper order to reconstruct it by calling
-      [load] below. *)
+  (* The call [inorder dict] evaluates in the list of entries in the
+     dictionary [dict], sorted in order, i.e., infix traversal of the
+     underlying ternary search tree. The function [inorder] is useful
+     when an optimal tree is designed and the corresponding entries
+     are needed in the proper order to reconstruct it by calling
+     [load] below. *)
+
   let inorder dict : 'def entry list =
     let rec post path entries = function
                 Nil -> entries
@@ -199,10 +207,11 @@ end
                       in post path' (post path (post path entries r) l) m
   in post [] [] dict
 
-  (** The compression of a standard ternary search tree consists in
-      compressing paths made only of [Leg] constructors with empty
-      side-trees, and replacing [Pre] nodes with an empty subtree by a
-      specific constructor [Def]. *)
+  (* The compression of a standard ternary search tree consists in
+     compressing paths made only of [Leg] constructors with empty
+     side-trees, and replacing [Pre] nodes with an empty subtree by a
+     specific constructor [Def]. *)
+
   let rec compress (p: char list) : 'def t -> 'def Opt.pre_t = function
     Leg (c,Nil,m,Nil) -> compress (c::p) m
   |                 t -> Opt.Cmp (rev_chars p, opti t)
@@ -238,37 +247,41 @@ and Opt : sig
   val thaw        : 'def t -> 'def Std.t
 end = struct
 
-  (** To save some memory, we can add a couple of value constructors
-      [Def] and [Cmp], to the type [t], which we rename [pre_t] in the
-      process. The structural constraints are the following:
-      {ol
-        {li The constructors [Def] and [Pre] can only occur as the
-          second argument of [Cmp] or the third of [Leg];}
-        {li The unary constructor [Nil] can only occur as the second or
+  (* To save some memory, we can add a couple of value constructors
+     [Def] and [Cmp], to the type [t], which we rename [pre_t] in the
+     process. The structural constraints are the following:
+
+       * The constructors [Def] and [Pre] can only occur as the
+         second argument of [Cmp] or the third of [Leg];
+
+       * The unary constructor [Nil] can only occur as the second or
          fourth argument of [Leg]; it is also best, although not
          mandatory, to use [Cmp] instead if [Nil] occurs in both
-         positions;}
-        {li the first argument of [Cmp] should not be [""];}
-        {li the second argument of [Pre] must either be [Leg] or [Cmp].}}
-  *)
+         positions;
+
+       * the first argument of [Cmp] should not be [""];
+
+       * the second argument of [Pre] must either be [Leg] or [Cmp].
+
+     The value [Def d] ("definition") is a tree containing only the
+     definition [d], that is, it represents the mapping from the empty
+     word to [d]. In fact, [Def d] is equivalent to [Pre(d,Nil)].
+
+     The value [Cmp (p,s)] ("compressed path") is a tree containing
+     words with the maximum prefix [p] in common, and whose suffixes
+     are in the subtree [s], that is, it denotes a dictionary whose
+     entries start with [p] and whose definitions are in [s]. It can
+     be understood as the compression of a linear path in the tree, so
+     [Comp(p,s)] is the same as [Leg(p.[0], Nil, Leg(p.[1], Nil, ...,
+     Leg(p.[n], Nil, s, Nil), Nil)...), Nil)], where [n =
+     String.length p]. *)
+
   type 'def pre_t =
     Nil
   | Pre of 'def * 'def pre_t
   | Leg of char * 'def pre_t * 'def pre_t * 'def pre_t
   | Def of 'def
-      (** The value [Def d] ({i definition}) is a tree containing only
-          the definition [d], that is, it represents the mapping from the
-          empty word to [d]. In fact, [Def d] is equivalent to
-          [Pre(d,Nil)].*)
   | Cmp of string * 'def pre_t
-      (** The value [Cmp (p,s)] ({i compressed path}) is a tree
-          containing words with the maximum prefix [p] in common, and
-          whose suffixes are in the subtree [s], that is, it denotes a
-          dictionary whose entries start with [p] and whose
-          definitions are in [s]. It can be understood as the
-          compression of a linear path in the tree, so [Comp(p,s)] is
-          the same as [Leg(p.[0], Nil, Leg(p.[1], Nil, ..., Leg(p.[n],
-          Nil, s, Nil), Nil)...), Nil)], where [n = String.length p]. *)
 
   type 'def t = int * 'def pre_t
 
--- Dict.mli
+++ Dict.mli
@@ -1,114 +1,132 @@
-(** The module {! Dict} ("dictionary") exports two submodules, {! Std}
-    and {! Opt}, whose signatures are those of a dictionary, that is,
-    a monotonic mapping from strings to definitions (no deletions).
+(* The module [Dict] ("dictionary") exports two submodules, [Std] and
+   [Opt], whose signatures are those of a dictionary, that is, a
+   monotonic mapping from strings to definitions (no deletions).
 
-    The module {! Opt} ("optimised") features dictionaries with faster
-    searches than those of module {! Std} ("standard"), but no
-    insertions. An optimised dictionary is obtained from a standard
-    one when the vocabulary is fixed, and it is always possible to
-    revert to the standard version. In other words, a dictionary from
-    module {! Opt} can be considered a constant and compact version of
-    {! Std}, with fast searches.
-*)
+     The module [Opt] ("optimised") features dictionaries with faster
+   searches than those of module [Std] ("standard"), but no
+   insertions. An optimised dictionary is obtained from a standard one
+   when the vocabulary is fixed, and it is always possible to revert
+   to the standard version. In other words, a dictionary from module
+   [Opt] can be considered a constant and compact version of [Std],
+   with fast searches.  *)
 
-(** The type {! word} is used in contexts where strings denote words in
-    the dictionary. *)
+(* The type [word] is used in contexts where strings denote words in
+   the dictionary. *)
+
 type word = string
 
-(** The type {! entry} represents entries in a dictionary of
-    definitions of type [def], that is, pairs of a word and a
-    definition. *)
+(* The type [entry represents entries in a dictionary of definitions
+   of type [def], that is, pairs of a word and a definition. *)
+
 type 'def entry = word * 'def
 
-(** The module {! Std} ("standard") provides a simple data type for
-    dictionaries. *)
-module rec Std : sig
+(* The module [Std] ("standard") provides a simple data type for
+   dictionaries. *)
 
-  (** The type {! t} denotes a dictionary, that is, a mapping
-      from words of type [string] to definitions of type [def]. *)
-type 'def t
+module rec Std :
+  sig
 
-(** The type {! dict} is an alias to be used when opening the
-    module {! Std}. *)
-type 'def dict = 'def t
+    (* The type [t] denotes a dictionary, that is, a mapping from
+       words of type [string] to definitions of type [def]. *)
 
-(** The empty dictionary. *)
-val empty : 'def t
+    type 'def t
 
-(** Let [char] be a character and [dict] a dictionary. If [dict]
-    contains words starting with [char], then the value of [suffix
-    char dict] is the dictionary containing those words {i without
-    their first letter}; if there are no such words, the value is the
-    empty dictionary. *)
-val suffix : char -> 'def t -> 'def t
+    (* The type [dict] is an alias to be used when opening the module
+       [Std]. *)
 
-(** If a dictionary [dict] contains a definition [def] of the word
-    [word], then the value of [find word dict] is [def], otherwise the
-    exception [Not_found] is raised. *)
-val find : word -> 'def t -> 'def
+    type 'def dict = 'def t
 
-(** If a dictionary [dict] contains a definition [def] for the word
-    [word], then the value of [find_opt word dict] is [Some def],
-    otherwise [None]. *)
-val find_opt : word -> 'def t -> 'def option
+    (* The empty dictionary. *)
 
-(** The value of [entries dict] is the list of the entries in the
-    dictionary [dict], sorted in alphabetic order. *)
-val entries : 'def t -> 'def entry list
+    val empty : 'def t
 
-(** The value of [definitions dict] is the list of definitions in the
-    dictionary [dict], sorted in alphabetic order.  It is equivalent
-    to [List.map snd (entries dict)].*)
-val definitions : 'def t -> 'def list
+    (* Let [char] be a character and [dict] a dictionary. If [dict]
+       contains words starting with [char], then the value of [suffix
+       char dict] is the dictionary containing those words {i without
+       their first letter}; if there are no such words, the value is
+       the empty dictionary. *)
 
-(** The call [add word def dict] evaluates in a dictionary containing
-    all the entries of the dictionary [dict], plus the entry [(word,
-    def)]. If [word] was already present in [dict], the exception
-    [Found word] ("redefinition") is raised, else, if the entry
-    [(word, def)] was already present in [dict], then [dict] is
-    returned (implying physical equality: [dict == add word def
-    dict]). *)
-val add : word -> 'def -> 'def t -> 'def t
+    val suffix : char -> 'def t -> 'def t
 
-(** The exception [Found w] is raised when adding the word [w] to a
-    dictionary where it is already defined. In other words, there is
-    no shadowing of previous entries by default. *)
-exception Found of word
+    (* If a dictionary [dict] contains a definition [def] of the word
+       [word], then the value of [find word dict] is [def], otherwise
+       the exception [Not_found] is raised. *)
 
-(** The call [load entries] evaluates in a dictionary containing
-    exactly the entries in [entries]. *)
-val load : 'def entry list -> 'def t
+    val find : word -> 'def t -> 'def
 
-(** Converting to the (space-)optimised representation (see module
-    [Opt]). The cross-module invariants are [Std.freeze (Opt.thaw d) =
-    d] and [Opt.thaw (Std.freeze d) = d] *)
-val freeze : 'def t -> 'def Opt.t
+    (* If a dictionary [dict] contains a definition [def] for the word
+       [word], then the value of [find_opt word dict] is [Some def],
+       otherwise [None]. *)
 
-(** (Maintainer) The call [inorder dict] evaluates in the list of
-    entries in the dictionary [dict], sorted in order, i.e., infix
-    traversal of the underlying ternary search tree. The function
-    [inorder] is useful when an optimal tree is designed and the
-    corresponding entries are needed in the proper order to reconstruct
-    it by calling [load] below. *)
-val inorder : 'def t -> 'def entry list
-end
+    val find_opt : word -> 'def t -> 'def option
 
-(** The module {! Opt} ("optimised") provides compact dictionaries. *)
-and Opt : sig
+    (* The value of [entries dict] is the list of the entries in the
+       dictionary [dict], sorted in alphabetic order. *)
 
-type 'def t
-type 'def dict = 'def t
+    val entries : 'def t -> 'def entry list
 
-val empty       : 'def t
-val suffix      : char -> 'def t -> 'def t
-val find        : word -> 'def t -> 'def
-val find_opt    : word -> 'def t -> 'def option
-val entries     : 'def t -> 'def entry list
-val definitions : 'def t -> 'def list
-val inorder     : 'def t -> 'def entry list
+    (* The value of [definitions dict] is the list of definitions in
+       the dictionary [dict], sorted in alphabetic order.  It is
+       equivalent to [List.map snd (entries dict)].*)
 
-(** Converting to the standard representation (see module [Std]).  The
-   cross-module invariants are [Std.freeze (Opt.thaw d) = d] and
-   [Opt.thaw (Std.freeze d) = d] *)
-val thaw : 'def t -> 'def Std.t
-end
+    val definitions : 'def t -> 'def list
+
+    (* The call [add word def dict] evaluates in a dictionary
+       containing all the entries of the dictionary [dict], plus the
+       entry [(word, def)]. If [word] was already present in [dict],
+       the exception [Found word] ("redefinition") is raised, else, if
+       the entry [(word, def)] was already present in [dict], then
+       [dict] is returned (implying physical equality: [dict == add
+       word def dict]). *)
+
+    val add : word -> 'def -> 'def t -> 'def t
+
+    (* The exception [Found w] is raised when adding the word [w] to a
+       dictionary where it is already defined. In other words, there
+       is no shadowing of previous entries by default. *)
+
+    exception Found of word
+
+    (* The call [load entries] evaluates in a dictionary containing
+       exactly the entries in [entries]. *)
+
+    val load : 'def entry list -> 'def t
+
+    (* Converting to the (space-)optimised representation (see module
+       [Opt]). The cross-module invariants are [Std.freeze (Opt.thaw
+       d) = d] and [Opt.thaw (Std.freeze d) = d] *)
+
+    val freeze : 'def t -> 'def Opt.t
+
+    (* (Maintainer) The call [inorder dict] evaluates in the list of
+       entries in the dictionary [dict], sorted in order, i.e., infix
+       traversal of the underlying ternary search tree. The function
+       [inorder] is useful when an optimal tree is designed and the
+       corresponding entries are needed in the proper order to
+       reconstruct it by calling [load] below. *)
+
+    val inorder : 'def t -> 'def entry list
+  end
+
+(* The module [Opt] ("optimised") provides compact dictionaries. *)
+
+and Opt :
+  sig
+
+    type 'def t
+    type 'def dict = 'def t
+
+    val empty       : 'def t
+    val suffix      : char -> 'def t -> 'def t
+    val find        : word -> 'def t -> 'def
+    val find_opt    : word -> 'def t -> 'def option
+    val entries     : 'def t -> 'def entry list
+    val definitions : 'def t -> 'def list
+    val inorder     : 'def t -> 'def entry list
+
+    (* Converting to the standard representation (see module [Std]).
+       The cross-module invariants are [Std.freeze (Opt.thaw d) = d]
+       and [Opt.thaw (Std.freeze d) = d] *)
+
+    val thaw : 'def t -> 'def Std.t
+  end
